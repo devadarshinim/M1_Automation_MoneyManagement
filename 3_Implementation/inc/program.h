9aa3401adaf73f4f27e11416e2002ebd03d8e6af
devadarshinim <devadarshinim52@gmail.com>
2021-11-22 18:39:44 +0530

    update

--- 3_Implementation/inc/program.h
+++ 3_Implementation/inc/program.h
@@ -1,7 +1,7 @@
-void display(char *name,char *email,char *gender,int age, char *password,int ammount)
+void display(char *name,char *email,char *gender,int age, char *password,int ammount);
 
-int compare(char a[],char b[]) 
+int compare(char a[],char b[]);
 
-int  withdraw(char *name,char *email,char *gender,int age, char *password,int amount,int withdrawammount)
+int  withdraw(char *name,char *email,char *gender,int age, char *password,int amount,int withdrawammount);
 
-int  deposit(char *name,char *email,char *gender,int age, char *password,int amount, int withdrawammount)
+int  deposit(char *name,char *email,char *gender,int age, char *password,int amount, int withdrawammount);
