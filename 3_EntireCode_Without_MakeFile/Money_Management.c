7f9431902b21b226733ef3748153d30395a2ead3
devadarshinim <devadarshinim52@gmail.com>
2021-11-24 19:46:12 +0530

    updated

--- 3_EntireCode_Without_MakeFile/Money_Management.c
+++ 3_EntireCode_Without_MakeFile/Money_Management.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
-#include "./inc/program.h"
 
+int compare(char a[],char b[]){
+    
+    int flag=0,i=0;  // integer variables declaration  
+    while(a[i]!='\0' &&b[i]!='\0')  // while loop  
+    {  
+       if(a[i]!=b[i])  
+       {  
+           flag=1;  
+           break;  
+       }  
+       i++;  
+    }  
+    if(flag==0)  
+    return 0;  
+    else  
+    return 1;  
+}
+int  withdraw(char *name,char *email,char *gender,int age, char *password,int amount,int withdrawammount){
+    int i;
+    if(amount<withdrawammount)
+    {
+        i=0;
+    }
+    else
+    {
+        i=amount- withdrawammount;
+    }
+    return i;
+}
+int deposit(char *name,char *email,char *gender,int age, char *password,int amount, int withdrawammount){
+    int j=amount+ withdrawammount;
+    return j;
+}
 int main(){
     printf("Welcome to Money management Sytem \n");
     int ch;
